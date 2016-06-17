15aefe485cbe82a46c33f4446287a77dfab50c4a
snowerdriesmac <Dries@MacBook-Air.local>
2016-06-17 22:39:26 +0200

    prod config.

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -21,6 +21,11 @@ module.exports = {
       compress: {
         warnings: false
       }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"'
+      }
     })
   ],
 
