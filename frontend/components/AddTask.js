2fb8d93edb641f7f99459efaef2decaa5064b0bd
snowerdriesmac <Dries@MacBook-Air.local>
2016-06-21 22:08:57 +0200

    clear input.

--- frontend/components/AddTask.js
+++ frontend/components/AddTask.js
@@ -3,7 +3,7 @@ import { browserHistory } from 'react-router';
 import AppBar from 'material-ui/AppBar';
 // import RaisedButton from 'material-ui/RaisedButton';
 // import * as apiActions from '../actions/api.js';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 import IconButton from 'material-ui/IconButton';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import FlatButton from 'material-ui/FlatButton';
@@ -16,16 +16,41 @@ const possibilities = [
   'Tafel afruimen',
 ];
 
+const mapStateToProps = function mapState() {
+  return {
+  };
+};
+
+const mapDispatchToProps = function mapProps() {
+  return {
+  };
+};
+
 class AddTask extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+    };
+  }
   _cancelAdd() {
-    browserHistory.push('/home');
+    if (this.state.searchText) {
+      this.setState({ searchText: '' });
+    } else {
+      browserHistory.push('/home');
+    }
+  }
+  _handleUpdateInput(input) {
+    this.setState({ searchText: input });
   }
   render() {
+    const cancelAdd = this._cancelAdd.bind(this);
+    const handleInputChange = this._handleUpdateInput.bind(this);
     return (
       <div className="container-fluid">
         <div className="row">
           <AppBar
-            iconElementLeft={<IconButton onTouchTap={this._cancelAdd}><NavigationClose /></IconButton>}
+            iconElementLeft={<IconButton onTouchTap={cancelAdd}><NavigationClose /></IconButton>}
             iconElementRight={<FlatButton label="Save" />}
             showMenuIconButton
             title="ADD TASK"
@@ -38,6 +63,10 @@ class AddTask extends React.Component {
               filter={AutoComplete.caseInsensitiveFilter}
               dataSource={possibilities}
               fullWidth
+              openOnFocus
+              searchText={this.state.searchText}
+              onUpdateInput={handleInputChange}
+              onNewRequest={handleInputChange}
             />
           </div>
         </div>
@@ -47,5 +76,5 @@ class AddTask extends React.Component {
 }
 
 AddTask.propTypes = {};
-
-export default AddTask;
+const ConnectedAddTask = connect(mapStateToProps, mapDispatchToProps)(AddTask);
+export default ConnectedAddTask;
