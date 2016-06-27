8baf5d1d24f1839551eac950fa612541f8e9b1ba
snowerdries <dries.nicolay@gmail.com>
2016-06-27 14:09:26 +0200

    Revert "Keepalive"
    This reverts commit f881daf47c2999182ef6cbc854774d751a924c57.

--- server.js
+++ server.js
@@ -49,12 +49,12 @@ if (isDevelopment) {
   });
 }
 
-/*var http = require("http");
+var http = require("http");
 var moment = require('moment');
 setInterval(function() {
     var hour=moment().hour();
     if(hour>=5 && hour <=23){
       http.get("http://snowerdries.herokuapp.com/");           
     }    
-}, 300000); // every 5 minutes (300000)*/
+}, 300000); // every 5 minutes (300000)
 
